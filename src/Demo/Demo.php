682fcb4d66150f7c7fab7d8024b7f90694635837
TheAnimeGuru <melanityt@gmail.com>
2021-05-12 21:41:38 +0200

    commit

--- src/Demo/Demo.php
+++ src/Demo/Demo.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 
-namespace DewaDonut\Demo;
+namespace AppflixDewaDonut\Demo;
 
-use DewaShop\Core\System\DataExtension;
-use DewaShop\Core\System\DataInterface;
+use AppflixDewaShop\Core\System\DataExtension;
+use AppflixDewaShop\Core\System\DataInterface;
 
 class Demo extends DataExtension implements DataInterface
 {
