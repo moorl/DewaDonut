a044706ee0d7007c70cc740ec4ffc07f2fcba1ba
TheAnimeGuru <melanityt@gmail.com>
2021-05-15 04:18:16 +0200

    Robins Funktionen hinzugefügt

--- src/Demo/Demo.php
+++ src/Demo/Demo.php
@@ -21,4 +21,20 @@ class Demo extends DataExtension implements DataInterface
     {
         return __DIR__;
     }
+    public function getStylesheets(): array
+    {
+        return [
+            'fonts/roboto-slab-latin.css'
+        ];
+    }
+
+    public function getRemoveQueries(): array
+    {
+        return [
+            "UPDATE `category` SET `cms_page_id` = NULL WHERE `id` = UNHEX('{NAVIGATION_CATEGORY_ID}');",
+            "DELETE FROM `cms_page` WHERE `id` = UNHEX('{DEMO_CMS_PAGE_1}');",
+            "DELETE FROM `cms_page` WHERE `id` = UNHEX('{DEMO_CMS_PAGE_2}');",
+            "UPDATE `sales_channel` SET `footer_category_id` = NULL, `footer_category_version_id` = NULL, `service_category_id` = NULL, `service_category_version_id` = NULL WHERE `id` = UNHEX('{SALES_CHANNEL_ID}');",
+        ];
+    }
 }
